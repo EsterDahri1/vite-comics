8f3029caad20dfbfcc66d0da242580e9df8a5367
EsterDahri <esterdahri@gmail.com>
2023-09-25 17:16:50 +0200

    complete all tasks

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <!--
-Descrizione:
+Descrizione 1:
 Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti necessari per strutturare il layout come da screenshot allegato. ✔️
 
-Quando la struttura a macroblocchi è pronta ✔️, popolate le voci di menu dinamicamente usando i data del componente.
+Quando la struttura a macroblocchi è pronta ✔️, popolate le voci di menu dinamicamente usando i data del componente. ✔️
 
-Per oggi diamo priorità alla struttura: quando è tutto bello solido, passiamo al Sass!
+Per oggi diamo priorità alla struttura: quando è tutto bello solido, passiamo al Sass! ✔️
 
 Bonus:
-Creare un componente aggiuntivo per gestire la fascia azzurra con le icone.
+Creare un componente aggiuntivo per gestire la fascia azzurra con le icone. ✔️
+
+Descrizione 2:
+create un nuovo componente che rappresenterà le card dei fumetti. ✔️
+Utilizzate i dati presenti nel file json che trovate in allegato e passateli al componente Card tramite props. ✔️
 -->
 
 <script>

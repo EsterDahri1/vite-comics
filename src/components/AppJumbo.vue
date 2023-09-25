97edbd815cfb3ad48b18105c461343c39debaae1
EsterDahri <esterdahri@gmail.com>
2023-09-25 13:38:41 +0200

    add bg jumbo and try add comics

--- src/components/AppJumbo.vue
+++ src/components/AppJumbo.vue
@@ -1,19 +1,27 @@
 <script>
+import comics from "../comics.js";
 export default {
     name: 'AppJumbo',
 
     data() {
         return {
-            message: '--> Content goes here <--'
+            comics
         }
     }
 }
 </script>
 
 <template>
-    <div class="bg-black">
-        <div class="container jumbotron d-flex justify-content-start align-items-center">
-            <h2>{{ message }}</h2>
+    <div class="teen pt-5 d-flex flex-column justify-content-end">
+        <div class="bg-black">
+            <div class="container jumbotron d-flex align-items-center justify-content-between">
+                <div class="col-2 h-100">
+                    <img :src="comics.thumb" class="card-img-top">
+                    <div class="card-body">
+                        <h4 class="text-muted text-uppercase text-white">{{ comics.series }}</h4>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -21,8 +29,15 @@ export default {
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
 
+.teen {
+    height: 800px;
+    background-image: url(../assets/img/jumbotron.jpg);
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+}
+
 .jumbotron {
     color: white;
-    height: 130px;
 }
 </style>
